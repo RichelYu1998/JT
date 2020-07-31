848b1deaa7fbb4a5617b457b19d62a6c89dd6a8d
余浩 <980187223@qq.com>
2020-07-31 15:43:29 +0800

    展现商品列表&动态生成_不分页

--- jt-manager/src/main/java/cn/tedu/service/ItemServiceImpl.java
+++ jt-manager/src/main/java/cn/tedu/service/ItemServiceImpl.java
@@ -3,6 +3,9 @@ package cn.tedu.service;
 import cn.tedu.mapper.ItemMapper;
 import cn.tedu.pojo.Item;
 import cn.tedu.vo.EasyUITable;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import net.sf.jsqlparser.expression.operators.relational.ItemsList;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +29,26 @@ public class ItemServiceImpl implements ItemService {
 	 *第N页
 	 SELECT * FROM tb_item LIMIT (n-1)ROWS,ROWS;
 	 */
-	@Override
+	/*@Override
 	public EasyUITable findItemByPage(Integer page, Integer rows) {
 		//参数1.记录总数   参数2: rows 当前页的记录数
 		long total=itemMapper.selectCount(null);
 		int startIndex=(page-1)*rows;
 		List<Item> itemList=itemMapper.findItemByPage(startIndex,rows);
 		return new EasyUITable(total,itemList);
-	}
+	}*/
+		/*2.利用MP机制 动态生成*/
+	@Override
+	public EasyUITable findItemByPage(Integer page, Integer rows) {
+		//Page 参数1：第几页 参数2：每页多少内容
+		QueryWrapper<Item> queryWrapper = new QueryWrapper<>();
+		queryWrapper.orderByDesc("updated");
+		IPage<Item> iPage = new Page<>(page,rows);
+		iPage = itemMapper.selectPage(iPage,queryWrapper);
 
+		Long total = iPage.getTotal();
+		List<Item> itemList = iPage.getRecords();
+		return new EasyUITable(total,itemList);
+
+	}
 }
