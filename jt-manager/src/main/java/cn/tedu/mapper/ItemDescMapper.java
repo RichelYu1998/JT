9ce2dd1daea9be5b737a836d6edcec8aa451de63
余浩 <980187223@qq.com>
2020-08-05 11:26:12 +0800

    商品表/商品分类表关系_准备

---
+++ jt-manager/src/main/java/cn/tedu/mapper/ItemDescMapper.java
@@ -0,0 +1,8 @@
+package cn.tedu.mapper;
+
+import cn.tedu.pojo.ItemDesc;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface ItemDescMapper extends BaseMapper<ItemDesc> {
+        //对象与表进行绑定.如果自己不写sql则可以省略mapper映射文件
+}
