c4c4646c3b0d36a19b26e476911fd09832787569
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-12 17:15:23 +0800

    json转化API&json转换为list

--- jt-manager/src/test/java/cn/tedu/TestObjectMapper.java
+++ jt-manager/src/test/java/cn/tedu/TestObjectMapper.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class TestObjectMapper {
     private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -23,6 +25,14 @@ public class TestObjectMapper {
             //2.将JSON转化为对象 利用反射机制实例化对象 利用get/set方法为对象赋值
             ItemDesc itemDesc2 = MAPPER.readValue(json, ItemDesc.class);
             System.out.println(itemDesc2.toString()); //只输出当前对象的数据
+            //3.将集合转换为JSON list
+            List<ItemDesc> list = new ArrayList<>();
+            list.add(itemDesc);
+            String listJSON = MAPPER.writeValueAsString(list);
+            System.out.println(listJSON);
+            //将json转换为list集合
+            List<ItemDesc> list2 = MAPPER.readValue(listJSON, list.getClass());
+            System.out.println(list2);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
