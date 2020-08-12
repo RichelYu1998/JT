550dee5e3d5e92244bd4a2b852264c780a62c139
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-12 15:01:56 +0800

    redis&是否存在key&setex&setparams

--- jt-manager/src/test/java/cn/tedu/TestRedis.java
+++ jt-manager/src/test/java/cn/tedu/TestRedis.java
@@ -2,6 +2,7 @@ package cn.tedu;
 
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
 
 public class TestRedis {
     /*
@@ -40,11 +41,12 @@ public class TestRedis {
             e.printStackTrace();
         }
     }
-    /*
-    * 需求说明
-    * 1.redis.set操作后面的操作会将之前的操作覆盖
-    * 要求：如果已经存在，则不允许赋值
-    * */
+    /**
+     * 需求说明:
+     *  1.redis.set操作,后面的操作会将之前的value覆盖
+     *  要求:如果key已经存在,则不允许赋值.
+     *  注意环境: 检查redis中是否有改数据.
+     *  */
     @Test
     public void test03(){
         Jedis jedis = new Jedis("192.168.126.129", 6379);
@@ -54,16 +56,33 @@ public class TestRedis {
         jedis.setnx("boss","马云");
         System.out.println(jedis.get("boss"));
     }
-    /*
-    *   为数据添加超时时间
-    * 原子性：一起完成一起回滚
-    * 锁机制：保证原子性 死锁~
-    * 小结：setnx：如果key存在 就不赋值
-    *      setex:保证原子性需求并设定超时时间
-    * */
+    /**
+     * 为数据添加超时时间.保证原子性操作
+     * 原子性: 一起完成一起回滚
+     * 锁机制: 保证原子性   死锁!!!!
+     * 小结: setnx 如果key存在则不赋值
+     *       setex  保证原子性操作,并且添加超时时间.
+     * * */
     @Test
     public void test04(){
         Jedis jedis = new Jedis("192.168.126.129", 6379);
        jedis.setex("aaa",20,"xxx");//满足原子性需求
+    }
+    /**
+     * 需求:
+     *  1.要求赋值操作时,如果数据已经存在则不允许赋值.
+     *  2.同时要求添加超时时间 并且满足原子性要求.
+     *   private static final String XX = "xx";   只有key存在时,才能赋值
+     *   private static final String NX = "nx";   只有key不存在时,赋值
+     *   private static final String PX = "px";   毫秒
+     *   private static final String EX = "ex";   秒
+     */
+    @Test
+    public void test05(){
+        Jedis jedis = new Jedis("192.168.126.129", 6379);
+        SetParams setParams=new SetParams();
+        setParams.nx().ex(20);
+        jedis.set("AAA","8888",setParams);
+        System.out.println(jedis.get("AAA"));
     }
 }
