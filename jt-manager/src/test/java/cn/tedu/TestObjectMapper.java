275b3212258b9bb02dfe4091c1209729ae4c6b28
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-12 17:10:13 +0800

    json转化API

--- jt-manager/src/test/java/cn/tedu/TestObjectMapper.java
+++ jt-manager/src/test/java/cn/tedu/TestObjectMapper.java
@@ -9,20 +9,20 @@ import java.util.Date;
 
 public class TestObjectMapper {
     private static final ObjectMapper MAPPER = new ObjectMapper();
-
     @Test
-    public void test01() {
+    public void test01(){
         ItemDesc itemDesc = new ItemDesc();
-        itemDesc.setItemId(100L).setItemDesc("测试JSON转换")
+        itemDesc.setItemId(100L)
+                .setItemDesc("测试JSON转化")
                 .setCreated(new Date())
                 .setUpdated(itemDesc.getCreated());
         try {
             //1.将java对象转化为JSON
             String json = MAPPER.writeValueAsString(itemDesc);
             System.out.println(json);
-            //2.将json转化为对象   传递需要转化之后的class类型   调用是对象的set方法
+            //2.将JSON转化为对象 利用反射机制实例化对象 利用get/set方法为对象赋值
             ItemDesc itemDesc2 = MAPPER.readValue(json, ItemDesc.class);
-            System.out.println(itemDesc2);
+            System.out.println(itemDesc2.toString()); //只输出当前对象的数据
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
