33662c3e7d76d09f7137eead0d6f975ca30e9b01
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-14 14:25:36 +0800

    AOP实现业务处理_1.3

--- jt-common/src/main/java/cn/tedu/aop/CacheAOP.java
+++ jt-common/src/main/java/cn/tedu/aop/CacheAOP.java
@@ -21,27 +21,25 @@ import javax.annotation.Resource;
 public class CacheAOP {
     @Resource
     private Jedis jedis;
+
     @SuppressWarnings("unchecked")
     @Around("@annotation(cacheFind)")
-    public Object around(ProceedingJoinPoint joinPoint,CacheFind cacheFind) {
-
+    public Object around(ProceedingJoinPoint joinPoint, CacheFind cacheFind) {
         //1.获取用户注解中的key     ITEM_CAT_LIST::0
         String key = cacheFind.key();
         //2.动态获取第一个参数当做key
         String firstArg = joinPoint.getArgs()[0].toString();
-        key += "::"+firstArg;
-
+        key += "::" + firstArg;
         Object result = null;
         //3.根据key查询redis.
-        if(jedis.exists(key)) {
-
+        if (jedis.exists(key)) {
             //根据redis获取数据信息
             String json = jedis.get(key);
             //如何获取返回值类型
             MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
             result = ObjectMapperUtil.toObject(json, methodSignature.getReturnType());
             System.out.println("aop查询redis缓存");
-        }else {
+        } else {
             //如果key不存在,则证明是第一次查询.  应该查询数据库
             try {
                 result = joinPoint.proceed(); //目标方法返回值
@@ -49,11 +47,11 @@ public class CacheAOP {
                 //将数据保存到redis中
                 String json = ObjectMapperUtil.toJSON(result);
                 int seconds = cacheFind.seconds();
-                if(seconds>0)
+                if (seconds > 0) {
                     jedis.setex(key, seconds, json);
-                else
+                } else {
                     jedis.set(key, json);
-
+                }
             } catch (Throwable e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
@@ -64,15 +62,14 @@ public class CacheAOP {
    /* //1.定义切入点表达式
     @Pointcut("bean(itemCatServiceImpl)")//只拦截xxx类中的方法
     public void pointcut(){
-
     }
     *//*
-    * 2.定义通知方法
-    * 需求
-    *   1.获取目标方法名称
-    *   2.获取目标方法对象
-    *   3.获取用户传输对象
-    * *//*
+     * 2.定义通知方法
+     * 需求
+     *   1.获取目标方法名称
+     *   2.获取目标方法对象
+     *   3.获取用户传输对象
+     * *//*
     @Before("pointcut()")
     public void before(JoinPoint joinPoint){
         System.out.println("我是前置通知");
