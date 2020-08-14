9a634f992fbe3a9a405cfdfd81f1d619b700670f
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-14 11:35:57 +0800

    AOP&前置通知_完善

--- jt-common/src/main/java/cn/tedu/aop/CacheAOP.java
+++ jt-common/src/main/java/cn/tedu/aop/CacheAOP.java
@@ -1,5 +1,6 @@
 package cn.tedu.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,8 +14,26 @@ public class CacheAOP {
     public void pointcut(){
 
     }
+    /*
+    * 2.定义通知方法
+    * 需求
+    *   1.获取目标方法名称
+    *   2.获取目标方法对象
+    *   3.获取用户传输对象
+    * */
     @Before("pointcut()")
-    public void before(){
+    public void before(JoinPoint joinPoint){
         System.out.println("我是前置通知");
+        //1.获取方法名称
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        //2.获取对象
+        Object target = joinPoint.getTarget();
+        //3.获取参数
+        Object[] objs = joinPoint.getArgs();
+        System.out.println("类名名称："+className);
+        System.out.println("方法名称："+methodName);
+        System.out.println("对象名称："+target);
+        System.out.println("方法参数："+objs);
     }
 }
