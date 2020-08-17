d8ddcc3e2761811fed5bf487376c840784f1c1d7
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-17 15:18:30 +0800

    开启主页controller

--- jt-web/src/main/java/cn/tedu/config/MvcConfigurer.java
+++ jt-web/src/main/java/cn/tedu/config/MvcConfigurer.java
@@ -1,4 +1,4 @@
-package cn.tedu.web.config;
+package cn.tedu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
