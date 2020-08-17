1304f2d24501c0039a5377d880b8be56bee2ecdc
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-17 16:03:00 +0800

    controller_UserController_register&login

--- jt-web/src/main/java/cn/tedu/controller/UserController.java
+++ jt-web/src/main/java/cn/tedu/controller/UserController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-    @RequestMapping("index")
-    public String index(){
-        return "index";
+    /*
+    * 业务说明：用一个方法实现页面的通用跳转
+    * http://www.jt.com/user/login.html 跳转页面 login.jsp
+    * http://www.jt.com/user/register.html 跳转页面 register.jsp
+    * */
+    @RequestMapping("register")
+    public String register(){
+        return "register";
+    }
+    @RequestMapping("login")
+    public String login(){
+        return "login";
     }
 }
