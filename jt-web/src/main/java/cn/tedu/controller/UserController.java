2e9598f7ee18f102ac758160612492ca27d40097
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-17 17:37:02 +0800

    restful&Controller

--- jt-web/src/main/java/cn/tedu/controller/UserController.java
+++ jt-web/src/main/java/cn/tedu/controller/UserController.java
@@ -8,21 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
     /*
-     * 业务说明：用一个方法实现页面的通用跳转
-     * http://www.jt.com/user/login.html 跳转页面 login.jsp
-     * http://www.jt.com/user/register.html 跳转页面 register.jsp
-     * */
-   /* @RequestMapping("register")
-    public String register(){
-        return "register";
-    }
-    @RequestMapping("login")
-    public String login(){
-        return "login";
-    }*/
+    * 业务说明：用一个方法实现页面的通用跳转
+    * http://www.jt.com/user/login.html 跳转页面 login.jsp
+    * http://www.jt.com/user/register.html 跳转页面 register.jsp
+    * */
     @RequestMapping("/{page}")
-    public String page(@PathVariable String page) {
+    public String page(@PathVariable String page){
         return page;
     }
 }
-
