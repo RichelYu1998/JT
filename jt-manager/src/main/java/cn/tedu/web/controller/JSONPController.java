34ecc2f0d42a7791818dbebdd3beff33aff72526
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-18 11:33:21 +0800

    JSONPController.java 更改为不出错的controller

--- jt-manager/src/main/java/cn/tedu/web/controller/JSONPController.java
+++ jt-manager/src/main/java/cn/tedu/web/controller/JSONPController.java
@@ -15,18 +15,19 @@ public class JSONPController {
      * url:http://manage.jt.com/web/testJSONP?callback=jQuery111101021758391465013_1597656788213&_=1597656788214
      * 规定:返回值结果,必须经过特殊的格式封装.callback(json)
      */
-    @RequestMapping("/web/testJSONP")
-    public JSONPObject jsonp(String callback) {
-        ItemDesc itemDesc = new ItemDesc();
+    /*@RequestMapping("/web/testJSONP")
+    public JSONPObject jsonp(String callback){
+        ItemDesc itemDesc=new ItemDesc();
         itemDesc.setItemId(101L).setItemDesc("我是商品详情信息");
-        return new JSONPObject(callback, itemDesc);
-    }
-    /*public String  jsonp(String callback){
+        return new JSONPObject(callback,itemDesc);
+    }*/
+    @RequestMapping("/web/testJSONP")
+    public String  jsonp(String callback){
         ItemDesc itemDesc = new ItemDesc();
         itemDesc.setItemId(101L).setItemDesc("我是商品详情信息");
         String json = ObjectMapperUtil.toJSON(itemDesc);
         //return callback+"({'id':'100','name':'tomcat猫'})";
         return callback +"("+json+")";
-    }*/
+    }
 
 }
