7020e817c46a481d46386c929a88b04488501003
RichelYu1998 <66730386+RichelYu1998@users.noreply.github.com>
2020-08-18 10:39:35 +0800

    跨域简单入门_pro

--- jt-manager/src/main/java/cn/tedu/web/controller/JSONPController.java
+++ jt-manager/src/main/java/cn/tedu/web/controller/JSONPController.java
@@ -1,17 +1,32 @@
 package cn.tedu.web.controller;
 
+import cn.tedu.pojo.ItemDesc;
+import cn.tedu.util.ObjectMapperUtil;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import jdk.nashorn.internal.scripts.JS;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController     //json字符串
 public class JSONPController {
+
     /**
      * 完成JSONP的调用
-     * url:http://manager.jt.com/web/testJSONP?callback=jQuery111101021758391465013_1597656788213&_=1597656788214
+     * url:http://manage.jt.com/web/testJSONP?callback=jQuery111101021758391465013_1597656788213&_=1597656788214
      * 规定:返回值结果,必须经过特殊的格式封装.callback(json)
      */
     @RequestMapping("/web/testJSONP")
-    public String  jsonp(String callback){
-        return callback+"({'id':'100','name':'tomcat猫'})";
+    public JSONPObject jsonp(String callback) {
+        ItemDesc itemDesc = new ItemDesc();
+        itemDesc.setItemId(101L).setItemDesc("我是商品详情信息");
+        return new JSONPObject(callback, itemDesc);
     }
+    /*public String  jsonp(String callback){
+        ItemDesc itemDesc = new ItemDesc();
+        itemDesc.setItemId(101L).setItemDesc("我是商品详情信息");
+        String json = ObjectMapperUtil.toJSON(itemDesc);
+        //return callback+"({'id':'100','name':'tomcat猫'})";
+        return callback +"("+json+")";
+    }*/
+
 }
