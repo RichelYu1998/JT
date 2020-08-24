cf6e5c44768f4711709a34aa60101067ae01ded6
980187223@qq.com <Syp950628>
2020-08-24 10:39:20 +0800

    拦截器的配置_完善_1.0

--- jt-web/src/main/java/cn/tedu/handler/UserInterceptor.java
+++ jt-web/src/main/java/cn/tedu/handler/UserInterceptor.java
@@ -1,5 +1,6 @@
 package cn.tedu.handler;
 
+import cn.tedu.util.CookieUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import redis.clients.jedis.JedisCluster;
@@ -14,33 +15,29 @@ import javax.servlet.http.HttpServletResponse;
 public class UserInterceptor implements HandlerInterceptor {
     @Resource
     private JedisCluster jedisCluster;
-    /*
-     * 实现pre方法
-     * 返回值说明：
-     *           return false 表示拦截 需要配合重定向一起使用
-     *           return true  表示放行
-     * 需求1：如果用户没有登录，则重定向到系统登录页面
-     * */
+    private static final String TICKET = "JT_TICKET";
+    /**
+     *  实现pre的方法
+     *  返回值说明:
+     *          return  false 表示拦截 需要配合重定向一齐使用
+     *          return  ture  表示放行
+     *  需求1: 如果用户没有登录,则重定向到系统登录页面
+     *  判断条件: 如何判断用户是否登录.  1.检查Cookie中是否有记录   2.Redis中是否有记录.
+     */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-        //业务实现  1.获取cookie
-        Cookie[] cookies = request.getCookies();
-        if(cookies !=null && cookies.length>0) {
-            for (Cookie cookie : cookies) {
-                if("JT_TICKET".equals(cookie.getName())){
-                    //如果equals则说明cookie是存在的.
-                    String ticket = cookie.getValue();
-                    //2.redis中是否有该记录  如果有记录则放行请求.
-                    if(jedisCluster.exists(ticket)) {
-                        //说明数据以及存在.可以放行
-                        return true;
-                    }
-                }
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie cookie = CookieUtil.getCookieByName(request, TICKET);
+        if(cookie != null){ //不为null.则表示用户可能登录.
+            String ticket = cookie.getValue(); //cookie中存储的是Redis的key ticket密钥
+            if(jedisCluster.exists(ticket)){
+                //如果redis中的数据存在,则说明用户已经登录.可以放行请求.
+                return true;
+            }else{
+                //Cookie中的记录与Redis中的记录不一致.应该删除Cookie中的数据.
+                CookieUtil.deleteCookie(TICKET, "/", "jt.com",response);
             }
         }
-        //重定向到用户登录页面
         response.sendRedirect("/user/login.html");
-        return false;
+        return false;   //表示拦截
     }
 }
