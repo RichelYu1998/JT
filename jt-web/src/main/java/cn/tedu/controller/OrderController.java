a10adf79166fc1e8fe160ccb9e4bdd17f006a58e
980187223@qq.com <Syp950628>
2020-08-24 16:30:42 +0800

    订单确认页面跳转_pro

--- jt-web/src/main/java/cn/tedu/controller/OrderController.java
+++ jt-web/src/main/java/cn/tedu/controller/OrderController.java
@@ -10,22 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+//1.业务名称与Controller对应即可
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+    //2.添加接口
     @Reference
-    private DubboCartService dubboCartService;
-    /*
-    * 跳转订单确认页面
-    * 1.url：http://http://www.jt.com/order/create.html
-    * 2.参数：无参 null
-    * 3.返回值 order-cart
-    * 4.页面取值 {carts}需要查询购物車信息，给页面返回
-    * */
-    @RequestMapping("/create")
+    private DubboCartService cartService;
+    /**
+     * 跳转订单确认页面
+     * 1.url:http://www.jt.com/order/create.html
+     * 2.参数: 没有参数 null
+     * 3.返回值: order-cart
+     * 4.页面取值  {carts}  需要查询购物车信息.,给页面返回
+     */
+    @RequestMapping("create")
     public String create(Model model){
         Long userId = UserThreadLocal.get().getId();
-        List<Cart> cartList = dubboCartService.findCartListByUserId(userId);
+        List<Cart> cartList = cartService.findCartListByUserId(userId);
         model.addAttribute("carts",cartList);
         return "order-cart";
     }
